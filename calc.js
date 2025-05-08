6e7d8d658d5647ac1b9354181dabe679cecc82b1
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-05-08 16:25:59 -0500

    Added logic to update display based on pressed numbers, and added logic for the backspace input

--- calc.js
+++ calc.js
@@ -1,10 +1,49 @@
 // Add query selectors for the calculator buttons and the display
 const numButtons = document.querySelectorAll('#num');
-const opButtons = document.querySelectorAll('#num');
+const opButtons = document.querySelectorAll('#op');
 const display = document.querySelector('.display');
+
+let result = '0';
 
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
         console.log(button.textContent);
+        if (display.textContent === result){
+            display.textContent = button.textContent;
+        } else {
+            display.textContent = display.textContent + button.textContent;
+        }
     })
 })
+
+opButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Handles deleting last entered digit from display
+        if( button.getElementsByTagName('img').length > 0) {
+            display.textContent = display.textContent.slice(0, -1);
+            if (display.textContent.length === 0) {
+                display.textContent = '0'
+            }
+        }
+        
+        // Handles clearing display
+
+
+        // Handles addition input
+
+
+        // Handles substraction input
+
+        
+        // Handles multiplication input
+
+
+        // Handles division input
+
+
+        // Handles equals input
+
+
+    })
+}
+)
