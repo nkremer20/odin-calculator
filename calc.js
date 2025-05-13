1c44401ff40c4f80f8fe691f48f8d7a0b3e8fefb
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-05-13 15:39:52 -0500

    Added mathOperation function to calculate outputs

--- calc.js
+++ calc.js
@@ -4,17 +4,41 @@ const opButtons = document.querySelectorAll('#op');
 const display = document.querySelector('.display');
 
 let result = '0';
+let op;
+
+function mathOperation(num1, num2, op) {
+    switch (op) {
+        case '+':
+            op = undefined;
+            return num1 + num2;
+        case '-':
+            op = undefined;
+            return num1 - num2;
+        case 'x':
+            op = undefined;
+            return num1 * num2;
+        case '÷':
+            op = undefined;
+            return num1/num2;
+    };
+};
 
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
         console.log(button.textContent);
-        if (display.textContent === result){
+        if (display.textContent === '0'){
             display.textContent = button.textContent;
+        } else if (op !== undefined){
+            if (display.textContent === '0') {
+                display.textContent = button.textContent;
+            } else {
+                display.textContent = display.textContent + button.textContent;
+            }
         } else {
             display.textContent = display.textContent + button.textContent;
-        }
-    })
-})
+        };
+    });
+});
 
 opButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -23,15 +47,20 @@ opButtons.forEach(button => {
             display.textContent = display.textContent.slice(0, -1);
             if (display.textContent.length === 0) {
                 display.textContent = '0'
-            }
-        }
+            };
+        };
         
         // Handles clearing display
         if (button.textContent === 'AC') {
-            display.textContent = '0';
-        }
+            result = '0'
+            display.textContent = result;
+        };
 
         // Handles addition input
+        if (button.textContent === "+") {
+            result = display.textContent;
+            op = '+';
+        }
 
 
         // Handles substraction input
